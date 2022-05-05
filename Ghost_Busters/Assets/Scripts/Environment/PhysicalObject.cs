27608c23565ef8173fc6106da8bee31eadccb2a0
Wambo <griffindonnelly@rocketmail.com>
2022-05-05 12:20:39 +0100

    Added possession script for physical objects
    Physical objects script can be found in Scripts/Environment

--- Ghost_Busters/Assets/Scripts/Environment/PhysicalObject.cs
+++ Ghost_Busters/Assets/Scripts/Environment/PhysicalObject.cs
@@ -4,8 +4,43 @@ using UnityEngine;
 
 public class PhysicalObject : Possessable
 {
+    [SerializeField] private float bounceInterval, bounceForce;
+
+    private Rigidbody rb;
+    private Vector3 bounceDirection;
+    private bool possessed = false;
+    private float timer;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     public override void Possess()
     {
-        Debug.Log("Possessed");
+        possessed = true;
+    }
+
+    public override void Unossess()
+    {
+        possessed = false;
+    }
+
+    private void Update()
+    {
+        if (possessed)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                timer = bounceInterval;
+
+                bounceDirection = Vector3.up;
+                bounceDirection = Quaternion.Euler(Random.Range(-45, 45), Random.Range(0, 360), 0) * bounceDirection;
+
+                rb.AddForce(bounceDirection * bounceForce, ForceMode.Impulse);
+            }
+        }
     }
 }

91e021bf975b74c8cb860f7a375769984d7be388
Wambo <griffindonnelly@rocketmail.com>
2022-05-06 10:54:32 +0100

    Revert "Finalised gameplay scene"
    This reverts commit 814eb97abee2f162e006e6746889325e3de13434.

--- Ghost_Busters/Assets/Scripts/Environment/TV.cs
+++ Ghost_Busters/Assets/Scripts/Environment/TV.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TV : Possessable
+public class TV : MonoBehaviour
 {
-    [SerializeField] private GameObject staticScreen;
-
-    public override void Possess()
+    // Start is called before the first frame update
+    void Start()
     {
-        staticScreen.SetActive(true);
+        
     }
 
-    public override void Unossess()
+    // Update is called once per frame
+    void Update()
     {
-        staticScreen.SetActive(false);
+        
     }
 }

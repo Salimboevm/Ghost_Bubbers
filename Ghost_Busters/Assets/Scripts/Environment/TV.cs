814eb97abee2f162e006e6746889325e3de13434
Wambo <griffindonnelly@rocketmail.com>
2022-05-06 09:45:54 +0100

    Finalised gameplay scene

--- Ghost_Busters/Assets/Scripts/Environment/TV.cs
+++ Ghost_Busters/Assets/Scripts/Environment/TV.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TV : MonoBehaviour
+public class TV : Possessable
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject staticScreen;
+
+    public override void Possess()
     {
-        
+        staticScreen.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Unossess()
     {
-        
+        staticScreen.SetActive(false);
     }
 }
